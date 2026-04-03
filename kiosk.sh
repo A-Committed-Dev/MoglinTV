60476ca7fa7d22fa545f73ffe0f2e05fc519a6f8
A-Committed-Dev <winther.dev@gmail.com>
2026-04-04 01:49:33 +0200

    update kiosk script to use 'chromium' instead of 'chromium-browser' and add additional flags

--- kiosk.sh
+++ kiosk.sh
@@ -2,9 +2,12 @@
 # Start Chromium in kiosk mode pointing at the Flask face server.
 # Add this to your desktop autostart or systemd user service.
 
+sleep 5
+
+
 FLASK_URL="${FLASK_URL:-http://localhost:5000}"
 
-exec chromium-browser --noerrdialogs \
+exec chromium --noerrdialogs \
   --disable-infobars \
   --disable-session-crashed-bubble \
   --check-for-update-interval=31536000 \
@@ -12,4 +15,7 @@ exec chromium-browser --noerrdialogs \
   --incognito \
   --disable-translate \
   --disable-features=TranslateUI \
+  --no-sandbox \
+  --ozone-platform=wayland \
+  --noerrdialogs \
   "$FLASK_URL"
